c43f7c55b5e09ce5cc497ad4bf9efbdeec9686b1
Emmad Siddiqui <50277414+EmmadSiddiqui@users.noreply.github.com>
2021-10-09 12:50:23 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="signup.css">
+    
+</head>
+<body>
+    <form>
+    <div class="container">
+
+        <h1><b>Hello!</b></h1><hr>
+
+      <button type="button" onclick="location.href='signup.php'">Sign Up</button> 
+      <button type="button" onclick="location.href='login.php'">Login</button>
+      
+    </div>
+    </form>
+</body>
+</html>
