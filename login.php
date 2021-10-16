4f94028853377538eb8449b53b9645fcb7b3d108
Emmad Siddiqui <50277414+EmmadSiddiqui@users.noreply.github.com>
2021-10-16 16:32:07 +0500

    Update login.php

--- login.php
+++ login.php
@@ -15,7 +15,7 @@
     
           <label for="email"><b>E-mail</b></label>
           <input type="email" placeholder="Enter your e-mail" name="email" required>
-    
+        
           <label for="password"><b>Password</b></label>
           <input type="password" placeholder="Enter your Password" name="password" required>
 
@@ -32,31 +32,41 @@
 
 <?php
 
-session_start();
+
 include("form.php");
-error_reporting(0);
 
 if(isset($_POST['subbtn']))
 {
 $email=$_POST['email'];
 $password=$_POST['password'];
 
-$_SESSION["status"]=false;
     
-$check_signup_email="SELECT * FROM signup WHERE email = '$email' && password='$password'";
+$check_signup_email="SELECT * FROM signup WHERE email = '$email'";
 $result = mysqli_query($conn, $check_signup_email);
 $count = mysqli_num_rows($result);
 
 if($count == 1){
-    $_SESSION['email']=$email;
+    $email_pass = mysqli_fetch_assoc($result);
+    $db_pass = $email_pass['password'];
+    $_SESSION['username'] = $email_pass['username'];
+    $pass_decode = password_verify($password,$db_pass);
+
+    if($pass_decode){
+        echo '<script>alert("Login Successful.")</script>';
+        header('location: home.php');
+    }else{
+        echo '<script>alert("Incorrect password.")</script>';
+    }
+
+  /*  $_SESSION['email']=$email;
     $_SESSION['password']=$password;
-    $_SESSION["status"]=true;
-    header('location: home.php');
-    exit();
+    exit(); */
 }
 else{
-    echo '<script>alert("Invalid email or password.")</script>';
+    echo '<script>alert("Invalid email.")</script>';
 }
 }
+
+
 
 ?>
